57b84daacb165c72348b1adafbde6154c8ca12a8
Felipe Lobo <felipeblobo@gmail.com>
2021-05-04 21:29:45 -0300

    alterando layout

--- script.js
+++ script.js
@@ -1,71 +1,74 @@
 const cards = document.querySelector("#container");
 const button = document.querySelector("#productsButton");
 button.addEventListener("click", showProducts);
-const form = document.querySelector('form');
-const addProductButton = document.getElementById('addProductButton');
+const form = document.querySelector("form");
+const addProductButton = document.getElementById("addProductButton");
 
+button.addEventListener("click", hideButton);
 addProductButton.addEventListener("click", hideForm);
 
-async function showProducts() {
+function showProducts() {
+  for (let id = 1; id < 21; id++) {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => res.json())
+      .then((json) => {
+        const productData = json;
 
-    for (let id = 1; id < 21; id++) {
-    
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`)
-    const productData = await res.json();
-    console.log(productData)
-        
-    const card = document.createElement('div');
-    card.setAttribute('class', `card${id}`);
-    cards.appendChild(card);
+        const card = document.createElement("div");
+        card.setAttribute("class", `card${id}`);
+        cards.appendChild(card);
 
-    const imageDiv = document.createElement('div');
-    card.appendChild(imageDiv);
-    const image = document.createElement('img');
-    image.setAttribute('src', productData.image);
-    imageDiv.appendChild(image);
-    imageDiv.setAttribute('id', 'imageDiv');
+        const imageDiv = document.createElement("div");
+        card.appendChild(imageDiv);
+        const image = document.createElement("img");
+        image.setAttribute("src", productData.image);
+        imageDiv.appendChild(image);
+        imageDiv.setAttribute("id", "imageDiv");
 
-    const title = document.createElement('h3');
-    title.innerText = productData.title;
-    card.appendChild(title);
-    
-    const description = document.createElement('p');
-    description.innerText =  productData.description;
-    card.appendChild(description);
+        const title = document.createElement("h3");
+        title.innerText = productData.title;
+        card.appendChild(title);
 
-    const priceCategory = document.createElement('div');
-    card.appendChild(priceCategory);
-    priceCategory.setAttribute('id', 'priceCategory');
-    const price = document.createElement('p');
-    const category = document.createElement('p');
+        const description = document.createElement("p");
+        description.innerText = productData.description;
+        card.appendChild(description);
 
-    price.innerText = '$ ' + productData.price;
-    priceCategory.appendChild(price);
-    
-    category.innerText = productData.category;
-    priceCategory.appendChild(category);
+        const priceCategory = document.createElement("div");
+        card.appendChild(priceCategory);
+        priceCategory.setAttribute("id", "priceCategory");
+        const price = document.createElement("p");
+        const category = document.createElement("p");
 
+        price.innerText = "$ " + productData.price;
+        priceCategory.appendChild(price);
+
+        category.innerText = productData.category;
+        priceCategory.appendChild(category);
+      });
   }
 }
 
-
-function addProduct () {
-  fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    title: '',
-                    price: '',
-                    description: '',
-                    image: '',
-                    category: ''
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+function addProduct() {
+  fetch("https://fakestoreapi.com/products", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "",
+      price: "",
+      description: "",
+      image: "",
+      category: "",
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
 }
 
 function hideForm() {
-    form.classList.toggle('hide');
+  form.classList.toggle("hide");
 }
+
+
+function hideButton() {
+    button.classList.toggle("hide");
+  }
+  
