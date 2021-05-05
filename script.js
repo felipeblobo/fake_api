fad7a9ce25d0bd5526d121e2155138c390815473
Felipe Lobo <felipeblobo@gmail.com>
2021-05-04 23:22:35 -0300

    desenvolvendo form

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const cards = document.querySelector("#container");
 const button = document.querySelector("#productsButton");
 button.addEventListener("click", showProducts);
-const form = document.querySelector("form");
-const addProductButton = document.getElementById("addProductButton");
-
 button.addEventListener("click", hideButton);
+
+const cards = document.querySelector("#container");
+
+const addProductButton = document.getElementById("addProductButton");
 addProductButton.addEventListener("click", hideForm);
 
+const newProductSave = document.getElementById("newProductSave");
+newProductSave.addEventListener("click", addProduct);
+
+
+
 function showProducts() {
+  
   for (let id = 1; id < 21; id++) {
     fetch(`https://fakestoreapi.com/products/${id}`)
       .then((res) => res.json())
@@ -48,27 +54,53 @@ function showProducts() {
   }
 }
 
-function addProduct() {
-  fetch("https://fakestoreapi.com/products", {
-    method: "POST",
-    body: JSON.stringify({
-      title: "",
-      price: "",
-      description: "",
-      image: "",
-      category: "",
-    }),
-  })
-    .then((res) => res.json())
-    .then((json) => console.log(json));
-}
-
 function hideForm() {
+  const form = document.querySelector("form");
   form.classList.toggle("hide");
 }
 
-
 function hideButton() {
-    button.classList.toggle("hide");
-  }
+  button.classList.toggle("hide");
+}
+
+//   Adicionando produtos
+
+function addProduct() {
+ 
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+const category = document.getElementById('category');
+const image = document.getElementById('imageUrl');
+
+formValidation();
+
+cards.innerHTML += 
+
+`<div class="card">
+<div id='imageDiv'><img src='${image.value}' alt='foto'></div>
+<h3>${title.value}</h3>
+<p class="description">${description.value}</p>
+<div id='priceCategory'>
+    <p>$ ${price.value}</p>
+    <p>${category.value}</p>
+</div>
+
+
+</div>`
+
+}
+
+
+function formValidation(event) {
+  event.preventDefault();
+ 
+  const title = document.getElementById('title');
+  const price = document.getElementById('price');
+  const description = document.getElementById('description');
   
+
+  if (title.value === '' || price.value === '' || description.value === '') {
+    alert('Preencha todos os campos!')
+  }
+}
